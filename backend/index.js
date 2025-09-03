704513c16d0f528b0ec86951bea83fe3da028eab
malkah04 <your-email@example.com>
2025-09-03 20:50:40 +0300

    fix: reorganize folder structure

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const categoryPath = require("./routes/categoryRoutes");
+require("dotenv").config();
 
+const app = express();
+
+app.use(cors());
+
+connectDB();
+
+app.use(express.json());
+app.use("/api/category", categoryPath);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("server running on port 5000");
+});
